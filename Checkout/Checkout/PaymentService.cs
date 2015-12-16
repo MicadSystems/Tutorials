11f50ce20b2cd43628bf326ccb1c971efcb21d32
Kevin Pluck <kevinp@micad.co.uk>
2015-12-16 15:24:07 +0000

    added test with annoying dependency

---
+++ Checkout/Checkout/PaymentService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Checkout
+{
+    public class PaymentService
+    {
+        readonly Random _random = new Random();
+        public bool Pay()
+        {
+            return _random.Next(100) >= 50;
+        } 
+    }
+}

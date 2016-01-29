659fd3dcc18ac82ca3d5bafc2feb47cf62a2a1e9
Kevin Pluck <kevinp@micad.co.uk>
2016-01-29 09:42:07 +0000

    Refactored to a flock of ducks

--- SimuDuck/SimuDuck/Program.cs
+++ SimuDuck/SimuDuck/Program.cs
@@ -1,64 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SimuDuck.Ducks;
 using SimuDuck.FlyBehaviors;
 
 namespace SimuDuck
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var mallard = new MallardDuck();
-            mallard.Display();
-            mallard.PerformSwim();
-            mallard.PerformQuack();
-            mallard.PerformFly();
-
-            Console.WriteLine();
-
-            var redhead = new RedheadDuck();
-            redhead.Display();
-            redhead.PerformSwim();
-            redhead.PerformQuack();
-            redhead.PerformFly();
-
-            Console.WriteLine();
-
-            var rubberDuck = new RubberDuck();
-            rubberDuck.Display();
-            rubberDuck.PerformSwim();
-            rubberDuck.PerformQuack();
-            rubberDuck.PerformFly();
-
-            Console.WriteLine();
-
-            var decoyDuck = new DecoyDuck();
-            decoyDuck.Display();
-            decoyDuck.PerformSwim();
-            decoyDuck.PerformQuack();
-            decoyDuck.PerformFly();
-
-            Console.WriteLine();
-
             var robotDuck = new RobotDuck();
-            robotDuck.Display();
-            robotDuck.PerformSwim();
-            robotDuck.PerformQuack();
-            robotDuck.PerformFly();
+
+            var flock = new List<Duck>
+            {
+                new MallardDuck(),
+                new RedheadDuck(),
+                new RubberDuck(),
+                new DecoyDuck(),
+                robotDuck
+            };
+
+            foreach (var duck in flock)
+            {
+                DoDuck(duck);
+                Console.WriteLine();
+            }
 
             Console.ReadLine();
-
 
             robotDuck.FlyBehavior = new Rockets();
 
-            robotDuck.PerformFly();
+            DoDuck(robotDuck);
 
             Console.ReadLine();
+            
+        }
 
+        private static void DoDuck(Duck duck)
+        {
+            duck.Display();
+            duck.PerformSwim();
+            duck.PerformQuack();
+            duck.PerformFly();
         }
     }
 }
